e336343446fe411a35057b19ce18e148a0320b46
corey.oconnor0 <Corey.OConnor001@umb.edu>
2019-07-30 15:44:21 -0400

    Fixing parallelization issues on WWindows

--- R/runCIE.R
+++ R/runCIE.R
@@ -565,7 +565,7 @@ generateHypTabs <- function(ents, rels, evidence, verbose=TRUE,
     
     D <- left_join(rels, evidence, by = c('trguid' = 'uid'))
     cluster <- new_cluster(numCores)
-    intoGroups <- suppressWarnings(D %>% partition(srcuid, cluster=cluster))
+    intoGroups <- suppressWarnings(partition(D, srcuid, cluster=cluster)
     
     intoGroups %>%
         cluster_assign_value("evidence", evidence) %>%
